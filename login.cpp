b1373a9d5827aabfe6a7098f08dbbdf9eed6100c
Snowdence <snowdence2911@gmail.com>
2020-06-22 21:07:23 +0700

    login source

---
+++ login.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int login(string username, string password)
+{
+    return 1;
+}
+int main()
+{
+    string username = "admin";
+    string password = "pass";
+    login(username, password);
+    return 0;
+}
